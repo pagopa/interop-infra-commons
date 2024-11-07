5dd2e5e4a2ca11dcf798b44b6560d95483250750
gfisicaro <giuseppe.fisicaro@digitouch.it>
2024-11-07 15:24:32 +0100

    Create the Terraform project to deploy the SLA dashboard on CloudWatch

---
+++ terraform/modules/sla-monitoring/98-variables.tf
@@ -0,0 +1,22 @@
+variable "env" {
+  type        = string
+  description = "Environment name"
+}
+
+variable "apigw_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
+
+variable "apigw_single_endpoint_name" {
+  description = "Name of the API Gateway"
+  type        = string
+}
+
+
+
+
+
+
+
+
