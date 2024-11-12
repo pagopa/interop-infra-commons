56e843454c4ed4c5ca6248a3520cd68bc618d559
gfisicaro <giuseppe.fisicaro@digitouch.it>
2024-11-12 17:44:13 +0100

    bug fix

--- terraform/modules/sla-monitoring/11-dashboards.tf
+++ terraform/modules/sla-monitoring/11-dashboards.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_dashboard" "apigw" {
-  dashboard_name = "${var.dashboard_prefix}apigw-${replace(var.apigw_name, ".", "-")}"
+  dashboard_name = "${var.dashboard_prefix}-apigw-${replace(var.apigw_name, ".", "-")}"
   dashboard_body = templatefile("${path.module}/assets/SLA-Monitoring-apigw.json", {
     Region     = data.aws_region.current.name
     ApiGwName  = var.apigw_name
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_dashboard" "apigw" {
 
 resource "aws_cloudwatch_dashboard" "single_endpoint" {
   count          = var.apigw_single_endpoint_name != "" ? 1 : 0
-  dashboard_name = "${var.dashboard_prefix}apigw-${replace(var.apigw_single_endpoint_name, ".", "-")}"
+  dashboard_name = "${var.dashboard_prefix}-apigw-single-endpoint-${replace(var.apigw_single_endpoint_name, ".", "-")}"
   dashboard_body = templatefile("${path.module}/assets/SLA-Monitoring-single_endpoint.json", {
     Region     = data.aws_region.current.name
     ApiGwName  = var.apigw_single_endpoint_name
