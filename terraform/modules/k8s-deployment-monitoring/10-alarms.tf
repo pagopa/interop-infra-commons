36310e6dc299d4da56b2b297be8348f2a67b0daf
roberto-mondello-dgt <roberto.mondello@digitouch.it>
2025-02-05 15:39:03 +0100

    fixed avg_memory threshold added kind and cronjob alarms

--- terraform/modules/k8s-deployment-monitoring/10-alarms.tf
+++ terraform/modules/k8s-deployment-monitoring/10-alarms.tf
@@ -1,5 +1,25 @@
+###############################################################################
+# LOCALS
+###############################################################################
+locals {
+  # ====== Deployment  ======
+  is_performance_alarm_required        = var.create_performance_alarm && var.kind == "Deployment"
+  is_pod_availability_alarm_required   = var.create_pod_availability_alarm && var.kind == "Deployment"
+  is_pod_readiness_alarm_required      = var.create_pod_readiness_alarm && var.kind == "Deployment"
+  is_app_logs_errors_alarm_required    = var.create_app_logs_errors_alarm && var.kind == "Deployment" && var.cloudwatch_app_logs_errors_metric_name != null && var.cloudwatch_app_logs_errors_metric_namespace != null
+
+  # ====== CronJob  ======
+  is_cronjob_performance_alarm_required = var.create_cronjob_performance_alarm && var.kind == "CronJob"
+  is_cronjob_app_logs_errors_alarm_required = var.create_cronjob_app_logs_errors_alarm && var.kind == "CronJob" && var.cronjob_cloudwatch_app_logs_errors_metric_name != null && var.cronjob_cloudwatch_app_logs_errors_metric_namespace != null
+}
+
+###############################################################################
+# DEPLOYMENT ALARMS
+###############################################################################
+
+# 1) AVG CPU (Deployment)
 resource "aws_cloudwatch_metric_alarm" "avg_cpu" {
-  count = var.create_performance_alarm ? 1 : 0
+  count = local.is_performance_alarm_required ? 1 : 0
 
   alarm_name        = format("k8s-%s-avg-cpu-%s", var.k8s_deployment_name, var.k8s_namespace)
   alarm_description = format("AVG CPU usage alarm for %s", var.k8s_deployment_name)
@@ -26,8 +46,9 @@ resource "aws_cloudwatch_metric_alarm" "avg_cpu" {
   tags = var.tags
 }
 
+# 2) AVG MEMORY (Deployment)
 resource "aws_cloudwatch_metric_alarm" "avg_memory" {
-  count = var.create_performance_alarm ? 1 : 0
+  count = local.is_performance_alarm_required ? 1 : 0
 
   alarm_name        = format("k8s-%s-avg-memory-%s", var.k8s_deployment_name, var.k8s_namespace)
   alarm_description = format("AVG memory usage alarm for %s", var.k8s_deployment_name)
@@ -46,7 +67,7 @@ resource "aws_cloudwatch_metric_alarm" "avg_memory" {
   statistic           = "Average"
   treat_missing_data  = "notBreaching"
 
-  threshold           = var.avg_cpu_alarm_threshold
+  threshold           = var.avg_memory_alarm_threshold
   period              = var.performance_alarms_period_seconds
   evaluation_periods  = var.alarm_eval_periods
   datapoints_to_alarm = var.alarm_datapoints
@@ -54,8 +75,9 @@ resource "aws_cloudwatch_metric_alarm" "avg_memory" {
   tags = var.tags
 }
 
+# 3) Unavailable pods (Deployment)
 resource "aws_cloudwatch_metric_alarm" "unavailable_pods" {
-  count = var.create_pod_availability_alarm ? 1 : 0
+  count = local.is_pod_availability_alarm_required ? 1 : 0
 
   alarm_name        = format("k8s-%s-unavailable-pods-%s", var.k8s_deployment_name, var.k8s_namespace)
   alarm_description = format("Unavailable pods alarm for %s", var.k8s_deployment_name)
@@ -64,7 +86,6 @@ resource "aws_cloudwatch_metric_alarm" "unavailable_pods" {
 
   comparison_operator = "GreaterThanOrEqualToThreshold"
   treat_missing_data  = "missing"
-  # TODO: pass as variables?
   threshold           = 1
   datapoints_to_alarm = 1
   evaluation_periods  = 5
@@ -72,7 +93,7 @@ resource "aws_cloudwatch_metric_alarm" "unavailable_pods" {
   metric_query {
     id          = "e1"
     label       = "Unavailable pods"
-    expression  = "m1-m2"
+    expression  = "m1 - m2"
     return_data = true
   }
 
@@ -87,7 +108,7 @@ resource "aws_cloudwatch_metric_alarm" "unavailable_pods" {
 
       metric_name = "kube_deployment_status_replicas"
       namespace   = "ContainerInsights"
-
+      
       dimensions = {
         ClusterName = var.eks_cluster_name
         Service     = var.k8s_deployment_name
@@ -119,8 +140,9 @@ resource "aws_cloudwatch_metric_alarm" "unavailable_pods" {
   tags = var.tags
 }
 
+# 4) Readiness pods (Deployment)
 resource "aws_cloudwatch_metric_alarm" "readiness_pods" {
-  count = var.create_pod_readiness_alarm ? 1 : 0
+  count = local.is_pod_readiness_alarm_required ? 1 : 0
 
   alarm_name        = format("k8s-%s-readiness-pods-%s", var.k8s_deployment_name, var.k8s_namespace)
   alarm_description = format("Readiness pods alarm for %s", var.k8s_deployment_name)
@@ -129,7 +151,7 @@ resource "aws_cloudwatch_metric_alarm" "readiness_pods" {
 
   comparison_operator = "GreaterThanOrEqualToThreshold"
   treat_missing_data  = "missing"
-  # TODO: pass as variables?
+    # TODO: pass as variables?
   threshold           = 1
   datapoints_to_alarm = 1
   evaluation_periods  = 5
@@ -137,7 +159,7 @@ resource "aws_cloudwatch_metric_alarm" "readiness_pods" {
   metric_query {
     id          = "e1"
     label       = "Not ready pods"
-    expression  = "m1-m2"
+    expression  = "m1 - m2"
     return_data = true
   }
 
@@ -184,8 +206,9 @@ resource "aws_cloudwatch_metric_alarm" "readiness_pods" {
   tags = var.tags
 }
 
+# 5) Application errors (Deployment)
 resource "aws_cloudwatch_metric_alarm" "app_errors" {
-  count = var.create_app_logs_errors_alarm && var.cloudwatch_app_logs_errors_metric_name != null && var.cloudwatch_app_logs_errors_metric_namespace != null ? 1 : 0
+  count = local.is_app_logs_errors_alarm_required ? 1 : 0
 
   alarm_name        = format("k8s-%s-errors-%s", var.k8s_deployment_name, var.k8s_namespace)
   alarm_description = format("Application errors alarm for %s", var.k8s_deployment_name)
@@ -197,6 +220,97 @@ resource "aws_cloudwatch_metric_alarm" "app_errors" {
 
   dimensions = {
     PodApp       = var.k8s_deployment_name
+    PodNamespace = var.k8s_namespace
+  }
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  statistic           = "Sum"
+  treat_missing_data  = "notBreaching"
+
+  threshold           = 1
+  period              = 60 # 1 minute
+  evaluation_periods  = 5
+  datapoints_to_alarm = 1
+
+  tags = var.tags
+}
+
+###############################################################################
+# CRONJOB ALARMS
+###############################################################################
+
+# 1) Performance CPU (CronJob)
+resource "aws_cloudwatch_metric_alarm" "cronjob_avg_cpu" {
+  count = local.is_cronjob_performance_alarm_required ? 1 : 0
+
+  alarm_name        = format("k8s-%s-cronjob-avg-cpu-%s", var.k8s_cronjob_name, var.k8s_namespace)
+  alarm_description = format("AVG CPU usage alarm for CronJob %s in %s", var.k8s_cronjob_name, var.k8s_namespace)
+
+  alarm_actions = var.sns_topics_arns
+
+  metric_name = "pod_cpu_utilization_over_pod_limit"
+  namespace   = "ContainerInsights"
+  dimensions = {
+    ClusterName = var.eks_cluster_name
+    Service     = var.k8s_cronjob_name
+    Namespace   = var.k8s_namespace
+  }
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  statistic           = "Average"
+  treat_missing_data  = "notBreaching"
+
+  threshold           = var.cronjob_avg_cpu_alarm_threshold
+  period              = var.cronjob_performance_alarms_period_seconds
+  evaluation_periods  = var.cronjob_alarms_eval_periods
+  datapoints_to_alarm = var.cronjob_alarms_datapoints
+
+  tags = var.tags
+}
+
+# 2) Performance Memory (CronJob)
+resource "aws_cloudwatch_metric_alarm" "cronjob_avg_memory" {
+  count = local.is_cronjob_performance_alarm_required ? 1 : 0
+
+  alarm_name        = format("k8s-%s-cronjob-avg-memory-%s", var.k8s_cronjob_name, var.k8s_namespace)
+  alarm_description = format("AVG memory usage alarm for CronJob %s in %s", var.k8s_cronjob_name, var.k8s_namespace)
+
+  alarm_actions = var.sns_topics_arns
+
+  metric_name = "pod_memory_utilization_over_pod_limit"
+  namespace   = "ContainerInsights"
+  dimensions = {
+    ClusterName = var.eks_cluster_name
+    Service     = var.k8s_cronjob_name
+    Namespace   = var.k8s_namespace
+  }
+
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  statistic           = "Average"
+  treat_missing_data  = "notBreaching"
+
+  threshold           = var.cronjob_avg_memory_alarm_threshold
+  period              = var.cronjob_performance_alarms_period_seconds
+  evaluation_periods  = var.cronjob_alarms_eval_periods
+  datapoints_to_alarm = var.cronjob_alarms_datapoints
+
+  tags = var.tags
+}
+
+# 3) Application errors (CronJob)
+resource "aws_cloudwatch_metric_alarm" "cronjob_app_errors" {
+  count = local.is_cronjob_app_logs_errors_alarm_required ? 1 : 0
+
+  alarm_name        = format("k8s-%s-cronjob-errors-%s", var.k8s_cronjob_name, var.k8s_namespace)
+  alarm_description = format("App logs errors alarm for CronJob %s in %s", var.k8s_cronjob_name, var.k8s_namespace)
+
+  alarm_actions = var.sns_topics_arns
+
+  metric_name = var.cronjob_cloudwatch_app_logs_errors_metric_name
+  namespace   = var.cronjob_cloudwatch_app_logs_errors_metric_namespace
+
+  dimensions = {
+    PodApp       = var.k8s_cronjob_name
     PodNamespace = var.k8s_namespace
   }
 
