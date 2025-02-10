346a62d09871a45b6f094037c471eae076656b89
Manuel Morini <151859827+mmorini-dxc@users.noreply.github.com>
2025-02-10 12:10:24 +0100

    update variables

--- terraform/modules/log-streaming/98-variables.tf
+++ terraform/modules/log-streaming/98-variables.tf
@@ -3,23 +3,34 @@ variable "env" {
   description = "Environment name"
 }
 
+variable "module_resource_prefix" {
+  type        = string
+  description = "Prefix for the module resources"
+}
+
 # Metrics and alarms configuration
 variable "sns_topic_name" {
   description = "Value of the sns topic name used to deliver the alarms"
   type        = string
 }
 
+variable "firehose_active_partition_count_percentage_threshold" {
+  description = "Firehose active partition count percentage threshold"
+  type        = number
+  default     = 60
+}
+
 # Cloudwatch log group configuration for AWS Data Firehose 
-variable "cloudwatch_log_group_name" {
+variable "firehose_cloudwatch_log_group_name" {
   description = "Firehose Cloudwatch log group name"
   type        = string
 }
-variable "cloudwatch_log_stream_name" {
+variable "firehose_cloudwatch_log_stream_name" {
   description = "Firehose Cloudwatch log stream name"
   type        = string
 }
 
-variable "cloudwatch_log_group_retention_in_days" {
+variable "firehose_cloudwatch_log_group_retention_in_days" {
   description = "Firehose Cloudwatch log group logs retention in days"
   type        = number
   default     = 14
@@ -50,6 +61,18 @@ variable "firehose_stream_name" {
   default     = "terraform-kinesis-firehose-extended-s3-test-stream"
 }
 
+variable "firehose_buffering_size_mb" {
+  type        = number
+  description = "AWS Data Firehose stream buffering size in MB"
+  default     = 5
+}
+
+variable "firehos_buffering_interval_seconds" {
+  type        = number
+  description = "AWS Data Firehose stream buffering interval in seconds"
+  default     = 300
+}
+
 variable "firehose_stream_tags" {
   type        = map(string)
   description = "AWS Data Firehose stream tags"
@@ -57,13 +80,31 @@ variable "firehose_stream_tags" {
 }
 
 # AWS S3 target bucket configuration
-variable "s3_bucket_prefix" {
+
+variable "s3_bucket_name" {
   type        = string
-  description = "S3 target bucket name prefix"
+  description = "AWS target S3 bucket name"
 }
 
+variable "s3_bucket_object_lock_enabled" {
+  type = bool
+  description = "Enable S3 bucket object lock"
+  default = false
+}
+
+variable "s3_bucket_object_lock_mode" {
+  type = string
+  description = "S3 bucket object lock mode"
+  default = "COMPLIANCE"
+
+  validation {
+    condition = contains(["GOVERNANCE", "COMPLIANCE"], var.s3_bucket_object_lock_mode)
+    error_message = "value must be either 'GOVERNANCE' or 'COMPLIANCE'"
+  }
+}
 variable "s3_bucket_tags" {
   type        = map(string)
   description = "AWS target S3 bucket tags"
   default     = {}
 }
+
