158d4b3a925ffbd74c271ed271c4f07ec8437346
Manuel Morini <151859827+mmorini-dxc@users.noreply.github.com>
2025-02-11 15:44:26 +0100

    rename

--- terraform/modules/log-streaming/13-cloudwatch-logs.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_cloudwatch_log_group" "firehose" {# TODO verificare formato se usato da firehose
-  name              = var.firehose_cloudwatch_log_group_name
-  retention_in_days = var.firehose_cloudwatch_log_group_retention_in_days
-}
-
-resource "aws_cloudwatch_log_stream" "firehose" {
-  name           = var.firehose_cloudwatch_log_stream_name
-  log_group_name = aws_cloudwatch_log_group.firehose.name
-}
