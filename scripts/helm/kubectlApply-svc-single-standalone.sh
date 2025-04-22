d063c231bf2a1ebee6f48bc295ae4f463648bc54
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-04-22 16:22:44 +0200

    edit option in helmTemplate-svc-single.sh and kubectlApply-svc-single-standalone.sh script, add option in kubectlDiff-svc-single-standalone.sh

--- scripts/helm/kubectlApply-svc-single-standalone.sh
+++ scripts/helm/kubectlApply-svc-single-standalone.sh
@@ -12,7 +12,7 @@ help()
         [ -i | --image ] File with microservice image tag and digest
         [ -o | --output ] Default output to predefined dir. Otherwise set to "console" to print template output on terminal
         [ -sd | --skip-dep ] Skip Helm dependencies setup
-        [ -dr | --dry-run ] Use Helm --dry-run=server option when templating
+        [ -etl | --enable-templating-lookup ] Enable Helm to run with the --dry-run=server option in order to lookup configmaps and secrets when templating
         [ -h | --help ] This help"
     exit 2
 }
@@ -24,7 +24,7 @@ enable_debug=false
 post_clean=false
 output_redirect=""
 skip_dep=false
-dry_run=false
+enable_templating_lookup=false
 images_file=""
 
 step=1
@@ -78,8 +78,8 @@ do
           step=1
           shift 1
           ;;
-        -dr | --dry-run)
-          dry_run=true
+        -etl | --enable-templating-lookup)
+          enable_templating_lookup=true
           step=1
           shift 1
           ;;
@@ -129,8 +129,8 @@ fi
 if [[ $skip_dep == true ]]; then
   OPTIONS=$OPTIONS" -sd "
 fi
-if [[ $dry_run == true ]]; then
-  OPTIONS=$OPTIONS" --dry-run=server "
+if [[ $enable_templating_lookup == true ]]; then
+  OPTIONS=$OPTIONS" --enable-templating-lookup "
 fi
 
 #HELM_TEMPLATE_CMD="$SCRIPTS_FOLDER/helmTemplate-svc-single.sh -e $ENV -m $microservice $OPTIONS"
