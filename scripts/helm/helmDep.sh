28fc5c8de7e756f7a064cb5e28f06621df6ca0b5
roberto-mondello-dgt <roberto.mondello@digitouch.it>
2025-06-26 17:32:27 +0200

    refactor helmDep

--- scripts/helm/helmDep.sh
+++ scripts/helm/helmDep.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-help()
-{
+help() {
     echo "Usage: 
         [ -u | --untar ] Untar downloaded charts
         [ -v | --verbose ] Show debug messages
@@ -10,9 +9,12 @@ help()
     exit 2
 }
 
-PROJECT_DIR=${PROJECT_DIR:-$(pwd)}
-ROOT_DIR=$PROJECT_DIR
+PROJECT_DIR="${PROJECT_DIR:-$(pwd)}"
+ROOT_DIR="$PROJECT_DIR"
+
 SCRIPTS_FOLDER="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+
 
 args=$#
 untar=false
@@ -23,33 +25,42 @@ for (( i=0; i<$args; i+=$step ))
 do
     case "$1" in
         -u| --untar )
-          untar=true
+            untar=true
           step=1
           shift 1
-          ;;
+            ;;
         -v| --verbose )
-          verbose=true
+            verbose=true
           step=1
           shift 1
-          ;;
+            ;;
         -h | --help )
-          help
-          ;;
+            help
+            ;;
         *)
-          echo "Unexpected option: $1"
-          help
+            echo "Unexpected option: $1"
+            help
 
-          ;;
+            ;;
     esac
 done
 
 function setupHelmDeps() 
 {
     untar=$1
+
+    cd "$ROOT_DIR"
+
+    if [[ $verbose == true ]]; then
+        echo "Creating directory charts"
+    fi
+    mkdir -p charts
+
+    if [[ $verbose == true ]]; then
+        echo "Copying Chart.yaml to charts"
+    fi
+    cp Chart.yaml charts/
     
-    cd $ROOT_DIR
-    
-    rm -rf charts
     echo "# Helm dependencies setup #"
     echo "-- Add PagoPA eks repos --"
     helm repo add interop-eks-microservice-chart https://pagopa.github.io/interop-eks-microservice-chart > /dev/null
@@ -62,31 +73,42 @@ function setupHelmDeps()
     if [[ $verbose == true ]]; then
         echo "-- Search PagoPA charts in repo --"
     fi
-    helm search repo interop-eks-microservice-chart > /dev/null
-    helm search repo interop-eks-cronjob-chart > /dev/null
+        helm search repo interop-eks-microservice-chart > /dev/null
+        helm search repo interop-eks-cronjob-chart > /dev/null
 
     if [[ $verbose == true ]]; then
         echo "-- List chart dependencies --"
     fi
-    helm dep list | awk '{printf "%-35s %-15s %-20s\n", $1, $2, $3}'
-    
+    helm dep list charts | awk '{printf "%-35s %-15s %-20s\n", $1, $2, $3}'
+
+    cd charts
+
     if [[ $verbose == true ]]; then
         echo "-- Build chart dependencies --"
     fi
     # only first time
-    #helm dep build 
+    #helm dep build
     dep_up_result=$(helm dep up)
     if [[ $verbose == true ]]; then
         echo $dep_up_result
     fi
 
-    if [[ $untar == true ]]; then
-        cd charts
-        for filename in *.tgz; do 
-            tar -xf "$filename" && rm -f "$filename";
-        done;
+    cd "$ROOT_DIR"
+    mkdir -p charts
+    
+    if [[ $untar == true ]]; then   
+        for filename in charts/charts/*.tgz; do 
+            [ -e "$filename" ] || continue
+            echo "Processing $filename"
+            basename_file=$(basename "$filename" .tgz)
+            chart_name="${basename_file%-*}"
+            target_dir="charts/$chart_name"
 
-        cd ..
+            echo "→ Extracting to $target_dir"
+            mkdir -p "$target_dir"
+            tar -xzf "$filename" -C "$target_dir" --strip-components=1
+            rm -f "$filename"
+        done    
     fi
 
     set +e
@@ -98,7 +120,7 @@ function setupHelmDeps()
     fi
     set -e
 
-    cd -
+    cd "$ROOT_DIR/charts"
     echo "-- Helm dependencies setup ended --"
     exit 0
 }
