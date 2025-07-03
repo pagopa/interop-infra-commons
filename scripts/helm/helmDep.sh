024faefc4726e8706fd9a8741c161b46959dd333
roberto-mondello-dgt <roberto.mondello@digitouch.it>
2025-07-03 11:58:05 +0200

    modified removing charts/charts method

--- scripts/helm/helmDep.sh
+++ scripts/helm/helmDep.sh
@@ -117,17 +117,11 @@ function setupHelmDeps()
             mv charts/charts/*.tgz charts/
         fi
     fi
-    # Remove empty temp charts directory if it exists and if it is empty
-    if [[ -d charts/charts && -z "$(ls -A charts/charts)" ]]; then
-        if [[ $verbose == true ]]; then
-            echo "Removing empty temp charts directory"
-        fi
-        rmdir charts/charts
-    else
-        if [[ $verbose == true ]]; then
-            echo "charts temp directory is not empty, not removing it"
-        fi
+    # Remove temp charts directory
+    if [[ $verbose == true ]]; then
+        echo "Removing charts/charts directory"
     fi
+    rm -rf charts/charts
 
     set +e
     # Install helm diff plugin, first check if it is already installed
