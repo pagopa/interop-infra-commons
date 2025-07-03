3694d7de510624c8ed59933f23d6981313a74468
Michele Delli Paoli <152869547+micheledellipaoli-pagopa@users.noreply.github.com>
2025-07-03 09:58:33 +0200

    fix: edit domain_name variable in rest-apigw-openapi module (#47)

--- terraform/modules/rest-apigw-openapi/21-dns.tf
+++ terraform/modules/rest-apigw-openapi/21-dns.tf
@@ -1,4 +1,6 @@
 resource "aws_api_gateway_base_path_mapping" "this" {
+  count = var.domain_name != null ? 1 : 0
+  
   stage_name  = aws_api_gateway_stage.env.stage_name
   domain_name = var.domain_name
   api_id      = aws_api_gateway_rest_api.this.id
