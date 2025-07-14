b8a03e2363a20852ed6e012456ef9db64233812f
micheledellipaoli-pagopa <michele.dellipaoli@pagopa.it>
2025-07-14 15:17:00 +0200

    add UUID as suffix to stored procedure name to avoid cuncurrency issues

--- terraform/modules/postgresql-user/scripts/redshift/delete_role.sql
+++ terraform/modules/postgresql-user/scripts/redshift/delete_role.sql
@@ -5,7 +5,7 @@ CREATE SCHEMA IF NOT EXISTS ${SCHEMA_NAME};
 \i ./v_generate_user_grant_revoke_ddl.sql
 
 -- Create a procedure to revoke all the privileges of the target_user and, then, to drop it 
-CREATE OR REPLACE PROCEDURE ${SCHEMA_NAME}.drop_user_with_revoke(target_user VARCHAR)
+CREATE OR REPLACE PROCEDURE ${SCHEMA_NAME}.drop_user_with_revoke_${UUID}(target_user VARCHAR)
 LANGUAGE plpgsql
 AS $$
 DECLARE 
@@ -37,4 +37,7 @@ BEGIN
 END $$;
 
 -- Execute the procedure
-CALL ${SCHEMA_NAME}.drop_user_with_revoke('${USERNAME}');
+CALL ${SCHEMA_NAME}.drop_user_with_revoke_${UUID}('${USERNAME}');
+
+-- Drop the procedure
+DROP PROCEDURE ${SCHEMA_NAME}.drop_user_with_revoke_${UUID}
