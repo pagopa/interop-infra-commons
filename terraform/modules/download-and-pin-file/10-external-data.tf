c1c98d0c4d5c500077b5c0e7d82b93b433b9dec4
Marco Vit <marco.vit@pagopa.it>
2025-09-01 13:27:48 +0200

    feat(download-and-pin-file): a new terraform moduel to download a file from 'curl supported location' and check downloaded resource sha256

---
+++ terraform/modules/download-and-pin-file/10-external-data.tf
@@ -0,0 +1,55 @@
+
+locals {
+  full_destination_path = (
+    var.destination_path != null
+    ? trimspace(var.destination_path)
+    : format("%s/file_cache/%s", path.module, trimspace(var.file_cache_key))
+  )
+}
+
+data "external" "refresh_mv_lambda_zip" {
+
+  program = ["bash", "${path.module}/scripts/download_and_check_lambda_archive.sh"]
+
+  query = {
+    url              = var.file_url
+    hex_sha256       = var.file_sha256_hex
+    destination_path = local.full_destination_path
+  }
+
+  lifecycle {
+
+    precondition {
+
+      # - This condition checks that exactly one of the two parameters is not null and not 
+      #   "whitespace only" string.
+      # - It works by summing the count of valid variables (1 if valid, 0 if not) and 
+      #   ensuring the total is exactly 1.
+      # - Nested if instead of && because terraform do not have short-circuit in logical 
+      #   operator evaluation and trimspace want a not-null input.
+      condition = (
+        (
+          (
+            var.destination_path != null 
+            ? ( trimspace(var.destination_path) != "" ? 1 : 0 )
+            : 0
+          )
+          +
+          ( 
+            var.file_cache_key != null 
+            ? ( trimspace(var.file_cache_key) != "" ? 1 : 0)
+            : 0
+          )
+        )
+        == 1
+      )
+
+      error_message = <<-EOT
+          Validation failed: Exactly one and not both of 'destination_path' or 'file_cache_key'
+                    must be specified and must not be a "whitespace only" string.
+        EOT
+    }
+
+  }
+
+}
