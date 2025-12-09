14880679379bb91c76a1e6a653876a1388e141f8
Manuel Morini <manuel.morini@digitouch.it>
2025-12-09 15:41:55 +0100

    rename

--- terraform/modules/argocd/02-locals.tf
+++ terraform/modules/argocd/02-locals.tf
@@ -20,9 +20,9 @@ locals {
 
   # Merge dei values: base + custom (se forniti)
   # Usa try() per evitare il ternario che causerebbe type mismatch se merged_values ha dei valori con tipi diversi da default_values_file
-  # Se il file mergiato esiste (custom values forniti), lo usa, altrimenti usa il default
-  merged_values_json = yamldecode(try(data.local_file.merged_values[0].content, local.default_values_file))
+  # Se il file mergiato esiste (custom values forniti), lo usa, altrimenti usa il default (secondo argomento di try)
+  merged_values = yamldecode(try(data.local_file.merged_values[0].content, local.default_values_file))
 
   # Final ArgoCD values
-  argocd_values = local.merged_values_json
+  argocd_values = local.merged_values
 }
