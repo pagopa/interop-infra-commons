18b6aeb407ae336ab14d1ae2aa1cc03dc5e25f7a
Manuel Morini <manuel.morini@digitouch.it>
2025-12-09 15:42:08 +0100

    switch trigger

--- terraform/modules/argocd/01-data-merge-values.tf
+++ terraform/modules/argocd/01-data-merge-values.tf
@@ -3,14 +3,19 @@
 resource "terraform_data" "merge_argocd_values" {
   count = local.should_merge_custom_values ? 1 : 0
 
+  # triggers_replace forza l'esecuzione del provisioner quando i file di input cambiano (sia creazione che update)
+  # Quando uno dei file cambia, Terraform distrugge e ricrea la risorsa terraform_data rieseguendo il provisioner
+  triggers_replace = {
+    base_file_hash     = md5(local.default_values_file)
+    override_file_hash = var.argocd_custom_values != null ? filemd5(var.argocd_custom_values) : ""
+  }
+
   input = {
     base_file_content = local.default_values_file
     override_file     = var.argocd_custom_values
-    trigger           = var.argocd_custom_values != null ? filemd5(var.argocd_custom_values) : ""
   }
 
   provisioner "local-exec" {
-    when    = create
     command = "${path.module}/scripts/merge-values.sh > ${local.merged_file_name}"
     
     environment = {
