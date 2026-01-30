2ae59ace79cefffb08f8360a1d7a0f1fc8927f47
Manuel Morini <manuel.morini@digitouch.it>
2026-01-30 09:27:31 +0100

    add argogrpc service

--- terraform/modules/argocd/50-argocd-instance.tf
+++ terraform/modules/argocd/50-argocd-instance.tf
@@ -48,9 +48,44 @@ resource "helm_release" "argocd" {
     value = var.argocd_create_crds ? "true" : "false"
   }
 
+  set {
+    name  = "configs.cm.url"
+    value = "https://${aws_route53_record.argocd_alb_alias[0].fqdn}"
+  }
+
   # Explicit dependency on the merged file (when present)
   # This ensures that the provisioner runs before reading the file
   depends_on = [
     data.local_file.merged_values
   ]
+}
+
+resource "kubernetes_service_v1" "argogrpc" {
+  metadata {
+    name      = "argocd-server-grpc"
+    namespace = var.argocd_namespace
+    labels = {
+      app = "argocd-server"
+    }
+
+    annotations = {
+      "alb.ingress.kubernetes.io/backend-protocol-version" = "GRPC"
+    }
+  }
+
+  spec {
+    type             = "ClusterIP"
+    session_affinity = "None"
+
+    selector = {
+      "app.kubernetes.io/name" = "argocd-server"
+    }
+
+    port {
+      name        = "grpc"
+      port        = 80
+      target_port = 8080
+      protocol    = "TCP"
+    }
+  }
 }
