325d46c29cfe80941829c58b7178a5570a57cad2
Manuel Morini <manuel.morini@digitouch.it>
2026-02-04 17:26:56 +0100

    add count condition

--- terraform/modules/argocd/51-alb.tf
+++ terraform/modules/argocd/51-alb.tf
@@ -4,9 +4,11 @@ locals {
 
 
 resource "aws_security_group" "argocd_alb_sg" {
+  count = local.setup_loadbalancer ? 1 : 0
+
   name        = format("alb/%s-argocd-%s", var.resource_prefix, var.env)
   description = "Allow HTTPS from VPN or internal CIDR"
-  vpc_id      = data.aws_eks_cluster.this.vpc_config[0].vpc_id
+  vpc_id      = data.aws_eks_cluster.this[0].vpc_config[0].vpc_id
 
   tags = {
     Name = format("alb/%s-argocd-%s", var.resource_prefix, var.env)
@@ -14,15 +16,19 @@ resource "aws_security_group" "argocd_alb_sg" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_https_from_vpc" {
-  security_group_id = aws_security_group.argocd_alb_sg.id
-  referenced_security_group_id = data.aws_security_group.vpn_clients.id
+  count = local.setup_loadbalancer ? 1 : 0
+
+  security_group_id = aws_security_group.argocd_alb_sg[0].id
+  referenced_security_group_id = data.aws_security_group.vpn_clients[0].id
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.argocd_alb_sg.id
+  count = local.setup_loadbalancer ? 1 : 0
+
+  security_group_id = aws_security_group.argocd_alb_sg[0].id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # all protocols
 }
@@ -42,7 +48,7 @@ resource "aws_lb" "argocd" {
   idle_timeout               = 300
 
   subnets         = var.private_subnet_ids
-  security_groups = [aws_security_group.argocd_alb_sg.id]
+  security_groups = [aws_security_group.argocd_alb_sg[0].id]
 
   tags = {
     Name        = format("%s-argocd-alb-%s", var.resource_prefix, var.env)
