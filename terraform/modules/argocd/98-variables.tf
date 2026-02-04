72111199c5d0d704ea6703a6ac469434725cc6fb
Manuel Morini <manuel.morini@digitouch.it>
2026-02-04 17:27:29 +0100

    add new variables

--- terraform/modules/argocd/98-variables.tf
+++ terraform/modules/argocd/98-variables.tf
@@ -2,6 +2,13 @@ variable "aws_region" {
   type        = string
   description = "AWS region"
 }
+
+variable "is_local_testing" {
+  type        = bool
+  description = "Set to true for local testing with kind/minikube. When false, uses EKS cluster credentials."
+  default     = false
+}
+
 variable "resource_prefix" {
   type        = string
   description = "Resource prefix used for resource naming"
@@ -24,6 +31,9 @@ variable "eks_cluster_name" {
   description = "Name of the EKS cluster accessing the ArgoCD cluster"
 }
 
+####################
+# ArgoCD variables #
+####################
 variable "argocd_custom_values" {
   type        = string
   description = "Path to a custom values file to override default ArgoCD configuration values."
@@ -65,7 +75,7 @@ variable "deploy_argocd" {
 variable "deploy_argocd_namespace" {
   type        = bool
   description = "Flag to determine whether to deploy the ArgoCD namespace."
-  default     = true
+  default     = false
 }
 
 variable "secret_prefix" {
@@ -86,15 +96,21 @@ variable "secret_recovery_window_in_days" {
   default     = 0
 }
 
+variable "use_aws_secrets_manager" {
+  type        = bool
+  description = "If true, use AWS Secrets Manager to generate and store admin credentials. If false, use argocd_admin_bcrypt_password and argocd_admin_password_mtime."
+  default     = true
+}
+
 variable "argocd_admin_bcrypt_password" {
   type        = string
-  description = "Optional override: bcrypt hash of ArgoCD admin password. If provided, AWS Secrets Manager resources are skipped. Use empty string to auto-generate from AWS Secrets Manager."
+  description = "Optional override: bcrypt hash of ArgoCD admin password. Used only when use_aws_secrets_manager=false."
   default     = ""
 }
 
 variable "argocd_admin_password_mtime" {
   type        = string
-  description = "Optional override: mtime string for ArgoCD admin password secret (e.g., RFC3339). If provided, AWS time_static is skipped. Use empty string to skip AWS time_static."
+  description = "Optional override: mtime string for ArgoCD admin password secret (e.g., RFC3339). Used only when use_aws_secrets_manager=false."
   default     = ""
 }
 
@@ -102,6 +118,57 @@ variable "argocd_helm_timeout_seconds" {
   type        = number
   description = "Timeout in seconds for Helm operations when deploying ArgoCD."
   default     = 600
+}
+
+############################
+# ArgoCD Plugins variables #
+############################
+variable "microservices_plugin_name" {
+  type        = string
+  description = "Name of the microservices plugin container"
+  default     = "argocd-plugin-microservices"
+}
+
+variable "microservices_plugin_image_prefix" {
+  type        = string
+  description = "Image registry prefix for microservices plugin"
+  default     = ""
+}
+
+variable "microservices_plugin_image_name" {
+  type        = string
+  description = "Image name for microservices plugin"
+  default     = "argocd-plugin-microservices"
+}
+
+variable "microservices_plugin_image_tag" {
+  type        = string
+  description = "Image tag for microservices plugin"
+  default     = "local"
+}
+
+variable "cronjobs_plugin_name" {
+  type        = string
+  description = "Name of the cronjobs plugin container"
+  default     = "argocd-plugin-cronjobs"
+}
+
+variable "cronjobs_plugin_image_prefix" {
+  type        = string
+  description = "Image registry prefix for cronjobs plugin"
+  default     = ""
+}
+
+variable "cronjobs_plugin_image_name" {
+  type        = string
+  description = "Image name for cronjobs plugin"
+  default     = "argocd-plugin-cronjobs"
+}
+
+variable "cronjobs_plugin_image_tag" {
+  type        = string
+  description = "Image tag for cronjobs plugin"
+  default     = "local"
 }
 
 #########################################################
@@ -138,15 +205,16 @@ variable "argocd_alb_name" {
   default     = null
 }
 
-
 variable "vpn_clients_security_group_id" {
   type        = string
   description = "ID of the VPN clients SG"
+  default     = null
 }
 
 variable "private_subnet_ids" {
   type        = list(string)
   description = "List of private subnet IDs for ALB placement"
+  default     = [ ]
 }
 
 #########################################################
@@ -156,7 +224,7 @@ variable "private_subnet_ids" {
 variable "create_argocd_project" {
   type        = bool
   description = "Flag to determine whether to create an ArgoCD AppProject"
-  default     = false
+  default     = true
 }
 
 variable "argocd_project_name" {
@@ -174,6 +242,12 @@ variable "argocd_project_description" {
 variable "argocd_project_source_repos" {
   type        = list(string)
   description = "List of Git repositories allowed for this project. Use ['*'] to allow all repositories."
+  default     = ["*"]
+}
+
+variable "argocd_project_source_namespaces" {
+  type        = list(string)
+  description = "List of source namespaces allowed for this project. Use ['*'] to allow all namespaces."
   default     = ["*"]
 }
 
@@ -234,6 +308,71 @@ variable "argocd_project_roles" {
   }))
   description = "List of roles to create for the ArgoCD project"
   default     = []
+}
+
+#########################################################
+# ArgoCD Repository variables                           #
+#########################################################
+
+variable "create_argocd_repository" {
+  type        = bool
+  description = "Flag to determine whether to create an ArgoCD repository credential"
+  default     = true
+}
+
+variable "argocd_repository_secret_name" {
+  type        = string
+  description = "Name of the Kubernetes secret that will store the repository credentials"
+  default     = "argocd-repository-creds"
+}
+
+variable "argocd_repository_type" {
+  type        = string
+  description = "Repository type: 'git' or 'helm'"
+  default     = "git"
+  validation {
+    condition     = contains(["git", "helm"], var.argocd_repository_type)
+    error_message = "Repository type must be either 'git' or 'helm'."
+  }
+}
+
+variable "argocd_repository_url" {
+  type        = string
+  description = "URL of the repository (e.g., https://github.com/myorg/myrepo.git or https://helm.example.com)"
+  default     = ""
+}
+
+variable "argocd_repository_username" {
+  type        = string
+  description = "Username for repository authentication (for HTTPS repos)"
+  default     = ""
+  sensitive   = true
+}
+
+variable "argocd_repository_password" {
+  type        = string
+  description = "Password or personal access token for repository authentication (for HTTPS repos)"
+  default     = ""
+  sensitive   = true
+}
+
+variable "argocd_repository_ssh_private_key" {
+  type        = string
+  description = "SSH private key for repository authentication (for SSH repos)"
+  default     = ""
+  sensitive   = true
+}
+
+variable "argocd_repository_insecure" {
+  type        = bool
+  description = "Allow insecure connections (skip TLS verification)"
+  default     = false
+}
+
+variable "argocd_repository_enable_lfs" {
+  type        = bool
+  description = "Enable Git LFS for the repository"
+  default     = false
 }
 
 #########################################################
