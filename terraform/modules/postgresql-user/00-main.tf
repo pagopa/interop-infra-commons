c52f5b1de1d4e507ec52f6afb3543321221bf4e6
Michele De Simone <michele.desimone@pagopa.it>
2026-03-02 09:53:18 +0100

    feat(postgresql-user): bump module requirements

--- terraform/modules/postgresql-user/00-main.tf
+++ terraform/modules/postgresql-user/00-main.tf
@@ -1,18 +1,12 @@
 terraform {
-  required_version = "~> 1.8.0"
+  required_version = ">~ 1.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.100.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6.3"
+      version = ">= 5.100.0"
     }
   }
-
 }
 
 data "aws_caller_identity" "current" {}
