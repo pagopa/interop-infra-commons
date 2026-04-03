d79e8de6baead3fe4c6400e3b72037d670541ff0
roberto-mondello-dgt <roberto.mondello@digitouch.it>
2026-04-03 19:11:13 +0200

    refactor VPN module removed unused files

--- terraform/modules/vpn/10-pki.tf
+++
@@ -1,2 +0,0 @@
-# External PKI only: the module consumes ACM certificate ARNs and does not create
-# or import any PKI material.
--- terraform/modules/vpn/11-secrets.tf
+++
@@ -1,1 +0,0 @@
-# External PKI only: no TLS material or Secrets Manager artifacts are created by the module.
