665ca73861744a1a3a3128cc74ca5d5abf59c648
Marco Vit <marco.vit@pagopa.it>
2026-04-13 18:34:06 +0200

    feature(PIN-9639,infra-pods)

---
+++ terraform/states/ecr/10-ecr.tf
@@ -0,0 +1,41 @@
+locals {
+  computed_repository_dir = "../../../apps/"
+  computed_repository_names = [
+    for d in distinct([
+      for f in flatten(fileset(path.root, "${local.computed_repository_dir}*/*")) :
+      dirname(f)
+    ]) :
+    "interop-infra-${replace(d, local.computed_repository_dir, "")}"
+  ]
+  custom_repository_names = []
+}
+
+resource "aws_ecr_repository" "app" {
+  for_each = toset(concat(local.computed_repository_names, local.custom_repository_names))
+
+  image_tag_mutability = contains(["test", "prod"], var.env) ? "IMMUTABLE" : "MUTABLE"
+  name                 = each.key
+}
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  for_each = { for repo in aws_ecr_repository.app : repo.name => repo if var.env == "dev" }
+
+  repository = each.value.name
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Delete untagged images"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 1
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
